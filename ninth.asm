3e4fe654cd5c216e1bdaa642d506353e96204b1a
Achal Agarwal <f2015436@pilani.bits-pilani.ac.in>
2018-03-07 17:46:49 +0530

    Ninth file

---
+++ ninth.asm
@@ -0,0 +1,27 @@
+.model tiny
+.CODE
+.startup
+  ;mov ax,2000h
+  ;mov ds,ax
+  mov si,1234h
+l1:
+  mov al,[si]
+  cmp al,0ffh
+  je nd
+cont:
+  call chek
+  mov [si],al
+  inc si
+  jmp l1
+nd:
+  .exit
+
+chek proc near
+  cmp al,15
+  jg i1
+  ret
+i1:
+  add al,20
+  ret
+chek endp
+end
